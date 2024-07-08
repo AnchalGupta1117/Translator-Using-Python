f20145381e0ec55165f7bf01bf77fbf29de48075
AnchalGupta1117 <mehakanchal1068@gmail.com>
2024-07-08 20:42:35 +0530

    importing

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk,messagebox
+import googletrans
+import textblob
+
+
+root=Tk()
+root.title("Google Translator")
+root.geometry("1080x400")
+root.configure(bg="white")
+root.mainloop()
