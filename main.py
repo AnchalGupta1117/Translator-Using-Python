cc7a3a7263f7ae2b20162d2e39e4fd28c9a4861a
AnchalGupta1117 <mehakanchal1068@gmail.com>
2024-07-08 20:44:42 +0530

    chnaging image

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ import textblob
 root=Tk()
 root.title("Google Translator")
 root.geometry("1080x400")
+
+#icon
+image_icon=PhotoImage(file="google.png")
+root.iconphoto(False,image_icon)
+
 root.configure(bg="white")
 root.mainloop()
