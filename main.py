99445cc72afe803b0aa9baa026f42c4885a7c1d4
AnchalGupta1117 <mehakanchal1068@gmail.com>
2024-07-09 14:47:33 +0530

    adding labels and more features

--- main.py
+++ main.py
@@ -12,5 +12,30 @@ root.geometry("1080x400")
 image_icon=PhotoImage(file="google.png")
 root.iconphoto(False,image_icon)
 
+language=googletrans.LANGUAGES
+languageV=list(language.values())
+lang1=language.keys()
+
+combo1=ttk.Combobox(root,values=languageV,font="Roboto 14",state="r")
+combo1.place(x=110,y=20)
+combo1.set("ENGLISH")
+
+label1=Label(root,text="ENGLISH",font="segoe 30 bold", bg="white", width=18,bd=5,relief=GROOVE)
+
+label1.place(x=10,y=50)
+
+f=Frame(root,bg="Black",bd=5)
+f.place(x=10,y=118,width=440,height=210)
+
+
+combo2=ttk.Combobox(root,values=languageV, font="RobotV 14",state="r")
+combo2.place(x=730,y=20)
+combo2.set("SELECT LANGUAGE")
+
+label2=Label(root,text="ENGLISH",font="segoe 30 bold", bg="white", width=18,bd=5,relief=GROOVE)
+
+label2.place(x=620,y=50)
+
+
 root.configure(bg="white")
 root.mainloop()
