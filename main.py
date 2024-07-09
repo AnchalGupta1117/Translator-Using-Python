ac50676f8b48901bb81ab8f7a7406a62396a2512
AnchalGupta1117 <mehakanchal1068@gmail.com>
2024-07-09 15:27:21 +0530

    added scroll bar and textarea

--- main.py
+++ main.py
@@ -27,6 +27,16 @@ label1.place(x=10,y=50)
 f=Frame(root,bg="Black",bd=5)
 f.place(x=10,y=118,width=440,height=210)
 
+text1=Text(f,font = "Robote 20", bg="white",relief=GROOVE,wrap=WORD)
+text1.place(x=0,y=0,width=430,height=200)
+
+scrollbar1=Scrollbar(f)
+scrollbar1.pack(side="right",fill="y")
+
+scrollbar1.configure(command=text1.yview)
+text1.configure(yscrollcommand=scrollbar1.set)
+
+
 
 combo2=ttk.Combobox(root,values=languageV, font="RobotV 14",state="r")
 combo2.place(x=730,y=20)
@@ -35,6 +45,18 @@ combo2.set("SELECT LANGUAGE")
 label2=Label(root,text="ENGLISH",font="segoe 30 bold", bg="white", width=18,bd=5,relief=GROOVE)
 
 label2.place(x=620,y=50)
+
+f1=Frame(root,bg="Black",bd=5)
+f1.place(x=620,y=118,width=440,height=210)
+
+text2=Text(f1,font = "Robote 20", bg="white",relief=GROOVE,wrap=WORD)
+text2.place(x=0,y=0,width=430,height=200)
+
+scrollbar2=Scrollbar(f1)
+scrollbar2.pack(side="right",fill="y")
+
+scrollbar2.configure(command=text2.yview)
+text2.configure(yscrollcommand=scrollbar2.set)
 
 
 root.configure(bg="white")
